a8c85171c9617dc32117a2c576980bc59c19489d
Kirin Tang <ktang91@my.bcit.ca>
2023-09-17 19:30:54 -0700

    handled user status check

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('signup', views.signup),
     path('login', views.login),
     path('logout', views.logout),
+    path('verify', views.check_user_status)
 ]
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,7 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.contrib.auth import authenticate
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken, TokenBackendError, TokenError
+from rest_framework_simplejwt.state import token_backend
 from rest_framework import status
 import environ
 import os
@@ -13,14 +14,6 @@ env = environ.Env()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ENV_PATH = os.path.join(BASE_DIR, '.env')
 environ.Env.read_env(ENV_PATH)
-
-def get_tokens_for_user(user):
-    refresh = RefreshToken.for_user(user)
-
-    return {
-        'refresh': str(refresh),
-        'access': str(refresh.access_token),
-    }
 
 @api_view(['POST'])
 def signup(request):
@@ -43,7 +36,8 @@ def login(request):
     token = get_tokens_for_user(user)
       
     res = Response()
-    res.set_cookie(key='jwt', value=token['access'], httponly=True)
+    res.set_cookie(key='jwt_access', value=token['access'], httponly=True)
+    res.set_cookie(key='jwt_refresh', value=token['refresh'], httponly=True)
     res.data = {
         'user': {
             'username': user.username,
@@ -53,28 +47,64 @@ def login(request):
     }
     return res
 
-# @api_view(['GET'])
-# def get_user(request):
-#     token = request.COOKIES.get('jwt')
+@api_view(['GET'])
+def check_user_status(request):
+    access_token = request.COOKIES.get('jwt_access')
+    is_refreshed = False
     
-#     if not token:
-#         raise AuthenticationFailed('Unauthorized')
+    if not access_token:
+        return Response({"error": "Access token is missing"}, status=status.HTTP_401_UNAUTHORIZED)
     
-#     try:
-#         payload = jwt.decode(token, env('JWT_SECRET'), algorithms='HS256')
-#     except jwt.ExpiredSignatureError:
-#         raise AuthenticationFailed('Unauthorized')
+    try:
+        token_data = token_backend.decode(access_token, verify=True) 
+    except TokenBackendError:
+        refresh_token = request.COOKIES.get('jwt_refresh')
+        
+        if not refresh_token:
+            return Response({"error": "Refresh token is missing"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        try:
+            refresh = RefreshToken(refresh_token)
+            access_token = str(refresh.access_token)
+            token_data = token_backend.decode(access_token, verify=True)
+            is_refreshed = True
+        except (TokenBackendError, TokenError):
+            return Response({'error': 'Invalid or expired tokens'}, status=status.HTTP_401_UNAUTHORIZED)
+     
+    user_id = token_data["user_id"]
     
-#     user = User.objects.filter(id=payload['uid']).first()
-#     serializer = UserSerializer(user)
+    try:
+        user = User.objects.get(id=user_id)
+    except User.DoesNotExist:
+            return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)   
+        
+    res = Response()
+    if is_refreshed:
+        res.set_cookie(key='jwt_access', value=access_token, httponly=True)
+    res.data = {
+        'user': {
+            'username': user.username,
+            'email': user.email,
+            'name': user.name,
+        }
+    }
     
-#     return Response(serializer.data)
+    return res
 
 @api_view(['POST'])
 def logout(_request):
     res = Response()
-    res.delete_cookie('jwt')
+    res.delete_cookie('jwt_access')
+    res.delete_cookie('jwt_refresh')
     res.data = {
         'message': 'Logout success'
     }
     return res
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
